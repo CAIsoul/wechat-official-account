b22868c5bea5b4cf8570205ca7c6b08db705a856
CAIsoul <caisoul92@gmail.com>
2024-10-25 13:11:26 +0800

    Add config ini file for credential.

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, request, abort
+import hashlib
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+Token = config.get('credential', 'token')
 
 app = Flask(__name__)
 
-@app.route('/wx')
+@app.route('/wx', methods=['GET'])
 def handle():
-    return "hello, this is handle view"
+    try:
+        data = request.args
+        if not data:
+            return "hello, this is handle view"
+        
+        signature = data.get('signature')
+        timestamp = data.get('timestamp')
+        nonce = data.get('nonce')
+        echostr = data.get('echostr')
+
+        token = Token
+        list_to_hash = [token, timestamp, nonce]
+        list_to_hash.sort()
+        sha1 = hashlib.sha1()
+
+        for item in list_to_hash:
+            sha1.update(item.encode('utf-8'))
+
+        hashcode = sha1.hexdigest()
+        print(f"handle/GET func: hashcode, signature: {hashcode}, {signature}")
+
+        if hashcode == signature:
+            return echostr
+        else:
+            return ""
+        
+    except Exception as e:
+        return str(e)
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
