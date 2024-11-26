4508391a18e2439368772debae4d6c37bbcdcfb2
CAIsoul <caisoul92@gmail.com>
2024-11-26 21:47:21 +0800

    Add some support for kimi AI.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+from flask import Flask, request
 import hashlib
 import configparser
 
--- util/chat-prompt.py
+++ util/chat-prompt.py
@@ -1,19 +1,47 @@
-from openai import OpenAI
+from openai import OpenAI 
 import configparser
+from pathlib import Path
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-API_KEY = config.get('chatgpt', 'apikey')
+CHAT_API_KEY = config.get('chatgpt', 'apikey')
+KIMI_API_KEY = config.get('kimi', 'apikey')
+AI_MODEL = 'moonshot-v1-8k'
+# AI_MODEL = 'gpt-4o-mini'
 
-client = OpenAI(api_key=API_KEY)
+def get_AI_client():
+    # client = OpenAI(api_key=CHAT_API_KEY)
+    client = OpenAI(
+        api_key = KIMI_API_KEY,
+        base_url = "https://api.moonshot.cn/v1",
+    )
+    
+    return client
 
-stream = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[{"role": "user", "content": "What do you know about Fuding?"}],
-    stream=True,
-)
+def helloworld():
+    client = get_AI_client()
+    stream = client.chat.completions.create(
+        model=AI_MODEL,
+        messages=[{"role": "user", "content": "What do you know about Fuding?"}],
+        stream=True,
+    )
 
-for chunk in stream:
-    if chunk.choices[0].delta.content is not None:
-        print(chunk.choices[0].delta.content, end="")
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            print(chunk.choices[0].delta.content, end="")
+
+
+async def create_speech():
+    client = get_AI_client()
+    speech_file_path = Path(__file__).parent / "speech.mp3"
+    response = client.audio.speech.create(
+    model="tts-1",
+    voice="alloy",
+    input="Sample speech testing."
+    )
+    response.stream_to_file(speech_file_path)
+
+
+# asyncio.run(create_speech())
+helloworld()
