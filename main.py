f1504d70315637a1ec9f856552c83a42a1799acd
CAIsoul <caisoul92@gmail.com>
2024-12-27 22:57:20 +0800

    Return basic message to subscribers who send a message.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, request
+from datetime import datetime
+import xml.etree.ElementTree as ET
 import hashlib
 import configparser
 
@@ -34,13 +36,63 @@ def handle():
         hashcode = sha1.hexdigest()
         print(f"handle/GET func: hashcode, signature: {hashcode}, {signature}")
 
-        if hashcode == signature:
-            return echostr
-        else:
-            return ""
+        if hashcode != signature:
+            return "Wrong Credential"
+
+        if request.method == 'POST':
+            return handlePostRequest(request.data)
+
+        return echostr
         
     except Exception as e:
         return str(e)
+
+
+def handlePostRequest(xmlData):
+    root = ET.fromstring(xmlData)
+
+    to_user_name = root.findtext('ToUserName')
+    from_user_name = root.findtext('FromUserName')
+    create_time = root.findtext('CreateTime')
+    msg_type = root.findtext('MsgType')
+    content = root.findtext('Content')
+    msg_id = root.findtext('MsgId')
+    msg_data_id = root.findtext('MsgDataId')
+    idx = root.findtext('Idx')
+
+    now = datetime.now()
+
+    # Convert to a timestamp
+    current_time_seconds = datetime.timestamp(now)
+
+    print(f"ToUserName: {to_user_name}")
+    print(f"FromUserName: {from_user_name}")
+    print(f"CreateTime: {create_time}")
+    print(f"MsgType: {msg_type}")
+    print(f"Content: {content}")
+    print(f"MsgId: {msg_id}")
+    print(f"MsgDataId: {msg_data_id}")
+    print(f"Idx: {idx}")
+
+    xml_string = """
+        <xml>
+            <ToUserName><![CDATA[{from_user_name}]]></ToUserName>
+            <FromUserName><![CDATA[{to_user_name}]]></FromUserName>
+            <CreateTime>{create_time}</CreateTime>
+            <MsgType><![CDATA[{msg_type}]]></MsgType>
+            <Content><![CDATA[{content}]]></Content>
+        </xml>
+    """.format(
+        to_user_name = to_user_name,
+        from_user_name = from_user_name,
+        create_time = current_time_seconds,
+        msg_type = msg_type,
+        content = "Hello World"
+    )
+
+    print(xml_string)
+    return xml_string
+
 
 
 if __name__ == '__main__':
